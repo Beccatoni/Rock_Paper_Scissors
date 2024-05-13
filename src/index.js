ccc184395582a45814cd2d3d996ba54e6cb318fe
Rebecca MUTONI <82598316+Beccatoni@users.noreply.github.com>
2024-05-13 18:06:11 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
-const buttons = document.querySelector('button');
-const 
+const buttons = document.querySelectorAll('button');
+const resultEl = document.getElementById('result');
+const player = document.getElementById('mine');
+const computer = document.getElementById('computers');
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', ()=> {
+        const result = playRound(button.id, computerPlay())
+        resultEl.textContent = result;
+    })
+}
+
+)
+
+function computerPlay(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length)
+    return choices[randomChoice];
+}
+
+
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        return `It's a tie!`;
+    } else if(
+        (playerSelection === 'rock' && computerSelection === "scissors") || 
+        (playerSelection === 'paper' && computerSelection === 'rock') || 
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ){
+        playerScore ++ ;
+        player.textContent = playerScore;
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else{
+        computerScore++;
+        computer.textContent = computerScore;
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
